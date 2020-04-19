1f19f17e8a88f11d08d181a18255539c63f4341b
Lucio Ruixo <lucioruixo@gmail.com>
2020-04-19 20:27:53 -0300

    Planet transparency and camera pivot selection added

---
+++ TP1 - Sistema Solar/Assets/Scenes/Solar System/Scripts/MainCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    const float InitialOffsetY = 700.0f;
+    const float InitialOffsetZ = -400.0f;
+
+    [HideInInspector] public int maxPivotNumber = 10;
+
+    public Vector3 offset = Vector3.zero;
+
+    public GameObject pivot;
+
+    public List<GameObject> pivots = new List<GameObject>();
+
+    void Start()
+    {
+        offset = new Vector3(0, InitialOffsetY, InitialOffsetZ);
+    }
+
+    void Update()
+    {
+        CheckInput();
+
+        transform.position = pivot.transform.position + offset;
+    }
+
+    void CheckInput()
+    {
+        for (int i = 0; i < maxPivotNumber; i++)
+        {
+            if (Input.GetKeyDown("[" + i.ToString() + "]"))
+                pivot = pivots[i];
+        }
+    }
+}
---
+++ TP1 - Sistema Solar/Assets/Scenes/Solar System/Scripts/Planet.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    const float InitialRotationSpeed = 15.0f;
+
+    const float TranslationSpeedBase = 0.5f;
+    const float MaxTranslationAngle = 360.0f;
+
+    public float rotationSpeed;
+
+    public float translationSpeed;
+    float traslationRadius;
+    float translationAngle = 0;
+    float newX;
+    float newZ;
+    Vector3 newPosition = new Vector3();
+
+    float transparencyValue = 0.25f;
+    Color color;
+
+    void Start()
+    {
+        rotationSpeed = InitialRotationSpeed;
+
+        traslationRadius = transform.localPosition.z;
+        translationSpeed = TranslationSpeedBase / transform.localScale.z;
+        newPosition = Vector3.zero;
+
+        color = GetComponent<Renderer>().material.color;
+    }
+
+    void FixedUpdate()
+    {
+        Rotate();
+        Translate();
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.Self);
+    }
+
+    void Translate()
+    {
+        translationAngle += translationSpeed * Time.deltaTime;
+        newX = traslationRadius * Mathf.Cos(translationAngle * Mathf.Deg2Rad);
+        newZ = traslationRadius * Mathf.Sin(translationAngle * Mathf.Deg2Rad);
+        newPosition.Set(newX, transform.localPosition.y, newZ);
+        transform.localPosition = newPosition;
+
+        if (translationAngle > MaxTranslationAngle)
+            translationAngle -= MaxTranslationAngle;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        color.a = transparencyValue;
+        GetComponent<Renderer>().material.color = color;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        color.a = 1.0f;
+        GetComponent<Renderer>().material.color = color;
+    }
+}
